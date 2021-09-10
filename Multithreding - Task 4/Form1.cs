5137b8d54853b389f2ce0434feb0a57e682980f9
Aqib Mukhtar <aqib.mukhtar@logiciel.com>
2021-09-10 03:55:01 -0700

    Task Completed

---
+++ Multithreding - Task 4/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multithreding___Task_4
+{
+    public partial class Form1 : Form
+    {
+        delegate int AsyncCallToGenerateRandomNumber();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var result = textBox1.BeginInvoke(new AsyncCallToGenerateRandomNumber(GenerateRandomNumber));
+            textBox1.Text = textBox1.EndInvoke(result).ToString();
+        }
+
+        int GenerateRandomNumber()
+        {
+            var rand = new Random();
+            return rand.Next(1, 6);
+        }
+    }
+}
